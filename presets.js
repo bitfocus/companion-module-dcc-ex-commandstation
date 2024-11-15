f6d5b9dac2634157b4c314cd213ab72c1fdd282c
Peter Daniel <peterdaniel@peters-macbook-air.local>
2024-11-15 23:34:37 +0000

    update presets

--- presets.js
+++ presets.js
@@ -1,55 +1,84 @@
-exports.get_presets = function (instanceLabel) {
-	var presets = []
-	
-		presets.push({
-			category: 'Power',
-			label: 'Main Power On & Off',
-			bank: {
-				style: 'text',
-				text: 'On Main',
-				size: '24',
-				color: 16777215,
-				bgcolor: 0,
-				latch: true
-			},
-			actions: [{
-				action: 'power',
-				options: {
-					selectedFunction: '1 MAIN',
-				}
-			}],
-			release_actions: [{
-				action: 'power',
-				options: {
-					selectedFunction: '0',
-				}
-			}],
-			feedbacks: [{
-				type: 'powerFeedback',
-				options: {
-					fg: 16777215,
-					bg: 6723840,
-				},
-			}],	
-		})
-		
-		presets.push({
-			category: 'Power',
-			label: 'All Stop',
-			bank: {
-				style: 'text',
-				text: 'All Stop',
-				size: '24',
-				color: 16777215,
-				bgcolor: 16711680
-			},
-			actions: [{
-				action: 'power',
-				options: {
-					selectedFunction: '!',
-				}
-			}],
-		})
+import { combineRgb } from '@companion-module/base'
 
-	return presets
+export function updatePresets() {
+	let presets = {}
+	let tracks = {}
+
+	tracks['main'] = { preset: 'Main Power', name: 'Main Power On & Off', label: 'Main On', on: '1 MAIN', off: '0 MAIN' }
+	tracks['prog'] = { preset: 'Prog Power', name: 'Prog Power On & Off', label: 'Prog On', on: '1 PROG', off: '0 PROG' }
+
+	for (let x in tracks) {
+		presets[tracks[x].preset] = {
+			type: 'button',
+			category: 'Power',
+			name: tracks[x].name,
+			style: {
+				text: tracks[x].label,
+				size: '24',
+				bgcolor: combineRgb(0, 0, 0),
+				color: combineRgb(255, 255, 255),
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'power',
+							options: {
+								power: tracks[x].on,
+							},
+						},
+					],
+					up: [],
+				},
+				{
+					down: [
+						{
+							actionId: 'power',
+							options: {
+								power: tracks[x].off,
+							},
+						},
+					],
+					up: [],
+				},
+			],
+			feedbacks: [
+				{
+					feedbackId: 'powerFeedback',
+					options: {
+						feedbackPowerState: tracks[x].on,
+					},
+					style: {
+						bgcolor: combineRgb(0, 128, 0),
+						color: combineRgb(255, 255, 255),
+					},
+				},
+			],
+		}
+	}
+
+	presets['All Stop'] = {
+		type: 'button',
+		category: 'Loco',
+		name: 'All Stop',
+		style: {
+			text: 'All Stop',
+			size: '24',
+			bgcolor: combineRgb(128, 0, 0),
+			color: combineRgb(255, 255, 255),
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'stop',
+						options: {},
+					},
+				],
+				up: [],
+			},
+		],
+	}
+
+	this.setPresetDefinitions(presets)
 }
