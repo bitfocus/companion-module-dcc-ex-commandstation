d51fd64ff6813a00a2fef18abc0b8eb523b2140b
Peter Daniel <peterdaniel@peters-macbook-air.local>
2024-11-17 16:55:23 +0000

    v2 updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { InstanceBase, InstanceStatus, Regex, runEntrypoint, TCPHelper } from '@companion-module/base'
 import { updateActions } from './actions.js'
-import { updateVariables } from './variables.js'
+// import { updateVariables } from './variables.js'
 import { updateFeedbacks } from './feedback.js'
 import { updatePresets } from './presets.js'
 
@@ -11,7 +11,7 @@ class DCCEX extends InstanceBase {
 		this.updateActions = updateActions.bind(this)
 		this.updateFeedbacks = updateFeedbacks.bind(this)
 		this.updatePresets = updatePresets.bind(this)
-		this.updateVariables = updateVariables.bind(this)
+		// this.updateVariables = updateVariables.bind(this)
 	}
 
 	getConfigFields() {
@@ -40,13 +40,13 @@ class DCCEX extends InstanceBase {
 				regex: Regex.Port,
 				required: true,
 			},
-			{
-				type: 'checkbox',
-				id: 'debuglog',
-				label: 'Show all return values in debug log',
-				width: 6,
-				default: false,
-			},
+			// {
+			// 	type: 'checkbox',
+			// 	id: 'debuglog',
+			// 	label: 'Show all return values in debug log',
+			// 	width: 6,
+			// 	default: false,
+			// },
 		]
 	}
 
@@ -65,12 +65,35 @@ class DCCEX extends InstanceBase {
 
 		this.config = config
 
-		this.stash = []
 		this.locos = []
+		this.locoVariables = []
 		this.powerState = null
 
+		this.variables = [
+			{
+				name: 'Device Model',
+				variableId: 'model',
+			},
+			{
+				name: 'Motor Shield',
+				variableId: 'motor',
+			},
+			{
+				name: 'DCC-EX Version',
+				variableId: 'version',
+			},
+			{
+				name: 'Power Status',
+				variableId: 'power',
+			},
+			{
+				name: 'Selected Loco DCC Address',
+				variableId: 'locoAddress',
+			},
+		]
+
 		this.updateActions()
-		this.updateVariables()
+		this.setVariableDefinitions(this.variables)
 		this.updateFeedbacks()
 		this.updatePresets()
 
@@ -89,7 +112,7 @@ class DCCEX extends InstanceBase {
 		this.config = config
 
 		this.updateActions()
-		this.updateVariables()
+		this.setVariableDefinitions(this.variables)
 		this.updateFeedbacks()
 		this.updatePresets()
 
@@ -125,6 +148,7 @@ class DCCEX extends InstanceBase {
 
 			this.socket.on('connect', () => {
 				this.log('info', 'Connected')
+				this.socket.receivebuffer = ''
 				// get command station status
 				this.sendCmd('<s>')
 				// poll every second
@@ -134,41 +158,32 @@ class DCCEX extends InstanceBase {
 
 			// separate buffered stream into lines with responses
 			this.socket.on('data', (chunk) => {
-				// this.log('debug', 'receiving: ' + chunk)
+				var i = 0
+				var line = ''
+				var offset = 0
 
-				var i = 0,
-					line = '',
-					offset = 0
-				this.receiveBuffer += chunk
+				// console.log("incomming:" + chunk)
 
-				while ((i = this.receiveBuffer.indexOf('\n', offset)) !== -1) {
-					line = this.receiveBuffer.substr(offset, i - offset)
-					offset = i + 1
+				this.socket.receivebuffer += chunk
+
+				// console.log("buffer:" + this.socket.receivebuffer)
+				// console.log('indexof:' + this.socket.receivebuffer.indexOf('\n'))
+
+				while ((i = this.socket.receivebuffer.indexOf('\n', offset)) !== -1) {
+					// console.log(i)
+					// console.log(offset)
+					// console.log('index of ' + this.socket.receivebuffer.indexOf('\n', offset))
+					line = this.socket.receivebuffer.substr(offset, i - offset)
+					// console.log(line)
+					offset = i + 2
+					this.socket.emit('receiveline', line.toString())
 				}
-
-				this.receiveBuffer = this.receiveBuffer.substr(offset)
+				this.socket.receivebuffer = this.socket.receivebuffer.substr(offset)
 			})
 
 			this.socket.on('receiveline', (line) => {
-				// if (this.command === null && line.match(/:/)) {
-				// 				this.command = line
-				// 			} else if (this.command !== null && line.length > 0) {
-				// 				this.stash.push(line.trim())
-				// 			} else if (line.length === 0 && this.command !== null) {
-				// 				var cmd = this.command.trim().split(/:/)[0]
-				// 				var obj = {}
-				// 				this.stash.forEach(function (val) {
-				// 					var info = val.split(/\s*:\s*/)
-				// 					obj[info.shift()] = info.join(':')
-				// 				})
-				//
+				// console.log('receiveline: ' + line)
 				this.processDeviceInformation(line)
-
-				// 	this.stash = []
-				// 	this.command = null
-				// } else if (line.length > 0) {
-				// 	console.log('weird response from device: ' + line.toString() + ' ' + line.length)
-				// }
 			})
 		}
 	}
@@ -182,6 +197,7 @@ class DCCEX extends InstanceBase {
 
 		if (line.length > 0) {
 			switch (line.substr(0, 1)) {
+				// examine first character only
 				case 'i': {
 					// command station info
 					this.log('info', line.substr(1).trim())
@@ -193,10 +209,7 @@ class DCCEX extends InstanceBase {
 				}
 				case 'l': {
 					// loco data broadcast
-					let locoArr = line.split(' ')
-					console.log(locoArr)
-					this.locos.push(locoArr)
-					console.log(this.locos)
+					this.updateLocoData(line)
 					break
 				}
 				case 'p': {
@@ -209,98 +222,77 @@ class DCCEX extends InstanceBase {
 				}
 				case 'X': {
 					// nothing defined
-					this.log('info', 'No data found')
+					this.log('info', 'Command failed')
 					break
+				}
+				default: {
+					this.log('debug', line)
 				}
 			}
 		}
 	}
 
-	// 	self.status(self.STATE_WARNING, 'Connecting');
-	//
-	// 	var options = { reconnect_interval: 5000 }
-	//
-	// 	if (self.config.host) {
-	// 		self.socket = new tcp(self.config.host, self.config.port, options);
-	//
-	// 		self.socket.on('status_change', function (status, message) {
-	// 			console.log(message)
-	// 			self.status(status, message);
-	// 		});
-	//
-	// 		self.socket.on('error', function (err) {
-	// 			console.log('Network error', err);
-	// 			self.status(self.STATE_ERROR, err);
-	// 			self.log('error','Network error: ' + err.message);
-	// 		});
-	//
-	// 		self.socket.on('connect', function () {
-	// 			self.status(self.STATE_OK);
-	// 			console.log('Connected');
-	//
-	// 			self.sendCmd('<s>')
-	// 		})
-	//
-	// 		self.socket.on('data', function (chunk) {
-	// 			console.log('DCC Received ' + chunk.length + ' bytes ', chunk)
-	//
-	// 			var i = 0, line = '', offset = 0;
-	// 			receivebuffer += chunk;
-	//
-	// 			// Split up Lines with new line and Process
-	// 			while ( (i = receivebuffer.indexOf('\n', offset)) !== -1) {
-	// 				line = receivebuffer.substr(offset, i - offset)
-	// 				offset = i + 1
-	// 				console.log(line.toString())
-	// 				line = line.replace('<','')
-	// 				line = line.replace('>','')
-	// 				if (self.config.debuglog === true) {
-	// 					self.log('debug','Received: ' + line)
-	// 				}
-	// 				self.socket.emit('receiveline', line.toString())
-	// 			}
-	// 			receivebuffer = receivebuffer.substr(offset)
-	// 		});
-	//
-	// 		self.socket.on('receiveline', function (line) {
-	// 			if (line.length > 0)
-	// 			{
-	// 				switch (line.substr(0,1))
-	// 				{
+	updateLocoData(rawData) {
+		//  <l cab reg speedByte functMap>
+		var locoArr = rawData.split(' ')
+		var locoVariableKey = 'locoThrottle_' + locoArr[1]
 
+		var speedText = this.decodeSpeedByte(locoArr[3])
 
+		// find loco in array and try to update
+		for (var i = 0; i < this.locos.length; i++) {
+			if (this.locos[i].address === locoArr[1]) {
+				// update existing
+				this.locos[i].rawspeed = locoArr[3]
+				this.locos[i].speed = speedText
+				this.locos[i].function = locoArr[4]
+				this.setVariableValues({ [locoVariableKey]: speedText })
+				// console.log(this.locos)
+				return
+			}
+		}
 
-	// 					case 'Q': {
-	// 						// sensors
-	// 						self.log('info','Sensor: ' + line.trim())
-	// 						break;
-	// 					}
-	// 					case 'q': {
-	// 						// sensors
-	// 						self.log('info','Sensor: ' + line.trim())
-	// 						break;
-	// 					}
-	// 					case 'H': {
-	// 						// turnouts
-	// 						self.log('info','Turnout: ' + line.trim())
-	// 						break;
-	// 					}
-	// 					case 'Y': {
-	// 						// output pins
-	// 						self.log('info','Output: ' + line.trim())
-	// 						break;
-	// 					}
-	// 					case 'c': {
-	// 						// track current
-	// 						self.log('info',line.substr(1).trim())
-	// 						break;
-	// 					}
-	// 					default:
-	// 						break;
-	// 				}
-	// 			}
-	// 		});
-	// 	}
+		// or if not found add new
+		var newLoco = { address: locoArr[1], rawspeed: locoArr[3], speed: speedText, function: locoArr[4] }
+		this.locos.push(newLoco)
+		// console.log(this.locos)
+
+		this.variables.push({
+			name: 'Loco ' + locoArr[1] + ' Throttle',
+			variableId: 'locoThrottle_' + locoArr[1],
+		})
+
+		// console.log(this.variables)
+
+		this.setVariableDefinitions(this.variables)
+		this.setVariableValues({ [locoVariableKey]: speedText })
+	}
+
+	decodeSpeedByte(value) {
+		console.log('decode: ' + value)
+
+		if (value == 0) {
+			// stop reverse
+			return '0 Stop (Reverse)'
+		}
+
+		if (value == 128) {
+			// stop forward
+			return '0 Stop (Forward)'
+		}
+
+		if (value > 1 && value < 128) {
+			// reverse speed
+			var speed = value - 1
+			return speed + ' Reverse'
+		}
+
+		if (value > 128) {
+			// forward speed
+			var speed = value - 129
+			return speed + ' Forward'
+		}
+	}
 
 	sendCmd(cmd) {
 		this.log('debug', 'sending: ' + cmd)
@@ -308,12 +300,6 @@ class DCCEX extends InstanceBase {
 		var cmd = unescape(cmd)
 		var end = '\r'
 
-		/*
-		 * create a binary buffer pre-encoded 'latin1' (8bit no change bytes)
-		 * sending a string assumes 'utf8' encoding
-		 * which then escapes character values over 0x7F
-		 * and destroys the 'binary' content
-		 */
 		var sendBuf = Buffer.from(cmd + end, 'latin1')
 
 		if (sendBuf !== undefined) {
